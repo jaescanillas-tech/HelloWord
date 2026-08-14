bf3ffab4417a836c963a41da707b79696a66874f
javier <ja.escanillas@duocuc.cl>
2026-08-14 02:07:39 -0400

    Commit Animal vrs 1.0

---
+++ src/main/java/dev/jaescanillas-tech/Animal.java
@@ -0,0 +1,50 @@
+package dev.jaescanillas;
+
+public class Mascota {
+    private String nombre;
+    private String tipoAnimal;
+    private String raza;
+    private double peso;
+
+    public Mascota(){
+        this.nombre = "Teddy";
+        this.tipoAnimal = "Perrito";
+        this.raza = "Puddle Toys";
+        this.peso = 3.2;
+    }
+
+    public Mascota(String nombre, String tipoAnimal, String raza, double peso){
+        this.nombre = nombre;
+        this.tipoAnimal = tipoAnimal;
+        this.raza = raza;
+        this.peso = peso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipoAnimal(String tipoAnimal) {
+        this.tipoAnimal = tipoAnimal;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public boolean vacuna(){
+        return true;
+    }
+
+    public void examen(String tipoExamen){
+
+    }
+
+    public void seguimiento(String fechaExamenes){
+
+    }
+}
