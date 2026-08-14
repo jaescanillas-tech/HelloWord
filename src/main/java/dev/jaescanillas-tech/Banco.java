a610f3021bfbedb2fb6398f408a6bb29ed83806f
javier <ja.escanillas@duocuc.cl>
2026-08-13 21:50:35 -0400

    Producto Banco terminado para revision

--- src/main/java/dev/jaescanillas-tech/Banco.java
+++ src/main/java/dev/jaescanillas-tech/Banco.java
@@ -1,1 +1,43 @@
-package dev.jaescanilla;
+package dev.jaescanillas;
+
+/*
+    Objeto creado de acuerdo al caso 3 de cuenta bancaria, contando con el numero de cuenta, el saldo y el tipo
+    de cuenta, los metodos de depositar si tiene dinero, mostrar el saldo y pagar en linea.
+ */
+
+public class CuentaBancaria {
+    private String tipo;
+    private String nombreTitular;
+    private Double saldo;
+    private  String numerocuenta;
+
+    public CuentaBancaria(){
+        this.tipo = "Corriente";
+        this.nombreTitular = "Juan Perez";
+        this.saldo = 3560.0;
+        this.numerocuenta = "1158742354852";
+    }
+
+    public CuentaBancaria(String tipo, String nombreTitular, Double saldo, String numerocuenta){
+        this.tipo = tipo;
+        this.nombreTitular = nombreTitular;
+        this.saldo = saldo;
+        this.numerocuenta = numerocuenta;
+    }
+
+    public Double deposit(Double saldo){
+        if (saldo != 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public String MostrarSaldo(){
+
+    }
+
+    public void PagarLinea(){
+
+    }
+}
