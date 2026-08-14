036929d4939d22564d9731c8674098d4dfaf3ba0
javier <ja.escanillas@duocuc.cl>
2026-08-13 22:10:50 -0400

    Commit Objeto Estudiante, falta ingresar Objeto curso.

---
+++ src/main/java/dev/jaescanillas-tech/Estudiante.java
@@ -0,0 +1,38 @@
+package dev.jaescanillas;
+
+public class Estudiante {
+    private String Nombre;
+    private String ApellidoP;
+    private String ApellidoM;
+    private String curso;
+
+    public Estudiante() {
+        this.Nombre = "Michael";
+        this.ApellidoP = "Larris";
+        this.ApellidoM = "Smith";
+        this.curso = "3 año";
+    }
+
+    public Estudiante(String Nombre, String ApellidoP, String ApellidoM, String curso) {
+        this.Nombre = Nombre;
+        this.ApellidoP = ApellidoP;
+        this.ApellidoM = ApellidoM;
+        this.curso = curso;
+    }
+
+    public boolean pasarCurso() {
+        return false;
+    }
+
+    public boolean repetircurso() {
+        return true;
+    }
+
+    public void Asignarcurso(){
+
+    }
+}
+
+public class Curso{
+
+}
