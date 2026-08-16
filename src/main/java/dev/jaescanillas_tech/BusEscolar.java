da3bd113f598beb760b839ec0b7f3a9766521ce0
javier <ja.escanillas@duocuc.cl>
2026-08-15 23:14:07 -0400

    Commit Bus Escolar

---
+++ src/main/java/dev/jaescanillas_tech/BusEscolar.java
@@ -0,0 +1,35 @@
+package dev.jaescanillas_tech;
+
+public class BusEscolar {
+    private String patente;
+    private String conductor;
+    private String destino;
+    private int capacidad;
+
+    public BusEscolar(){
+        this.patente = "AB12CS";
+        this.conductor = "Diego Larrys";
+        this.destino = "Colegio Rosario Concha";
+        this.capacidad = 15;
+    }
+
+    public BusEscolar(String patente, String conductor, String destino, int capacidad) {
+        this.patente = patente;
+        this.conductor = conductor;
+        this.destino = destino;
+        this.capacidad = capacidad;
+    }
+
+    public void ingresarDestino(){
+
+    }
+
+    public void ingresoEstudiantes(){
+
+    }
+
+    public void mostrarinformacion(){
+
+    }
+
+}
