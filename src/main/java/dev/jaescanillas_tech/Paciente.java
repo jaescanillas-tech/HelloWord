910205bd01ca593b900418610e5fb24bd0cded3f
javier <ja.escanillas@duocuc.cl>
2026-08-15 23:14:22 -0400

    Commit paciente

---
+++ src/main/java/dev/jaescanillas_tech/Paciente.java
@@ -0,0 +1,37 @@
+package dev.jaescanillas_tech;
+
+import java.util.random.RandomGenerator;
+
+public class Paciente {
+    private String run;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+
+    public Paciente(){
+        this.run = "12.123.123.K";
+        this.nombre = "Camila";
+        this.apellidoP = "Simpson";
+        this.apellidoM = "colon";
+    }
+
+    public Paciente(String run, String nombre, String apellidoP, String apellidoM) {
+        this.run = run;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+    }
+
+    public boolean Atencion(){
+        return true;
+    }
+
+    public void Prioridad(){
+
+    }
+
+    public void EntregaRemedios(){
+
+    }
+
+}
